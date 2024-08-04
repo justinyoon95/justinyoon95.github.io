166b6a52c241dd0a2b7f8f27f69aa0202eca6acd
Justin Yoon <justinyoon95s2@gmail.com>
2024-08-04 15:06:24 -0400

    This website is now goofy until it is done.

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={`${process.env.PUBLIC_URL}/dgyoontv.png`} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hi, this is Justin. And right up there, That is my roblox character. Cute isn't it?<br></br>
+           Well, this website is underconstruction right now so... see you soon!<br></br>
+           You might as well click "Learn React" to learn how this is made.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
           target="_blank"
           rel="noopener noreferrer"
         >
